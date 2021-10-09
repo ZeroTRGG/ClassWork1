2d14cdc518960100baf9d600c71651e597454b64
Zero <90512731+ZeroTRGG@users.noreply.github.com>
2021-10-09 16:59:30 +0300

    Add files via upload

---
+++ soda.py
@@ -0,0 +1,14 @@
+import random
+class Soda:
+        def __init__(self, add):
+                if isinstance(add, str):
+                        self.add = ingredient
+                else:
+                        self.add = None
+
+        def show_my_drink(self):
+                if self.add:
+                        print(f'Газировка и {self.add}')
+                else:
+                        print('Обычная гозировка')
+     
