95383cffaabc66dddd4fae887d53c3e6ad4d8608
Zero <90512731+ZeroTRGG@users.noreply.github.com>
2021-10-09 17:48:26 +0300

    Update soda.py

--- soda.py
+++ soda.py
@@ -1,14 +1,20 @@
-import random
 class Soda:
         def __init__(self, add):
                 if isinstance(add, str):
-                        self.add = ingredient
+                    self.add = add
                 else:
-                        self.add = None
+                    self.add = None
 
         def show_my_drink(self):
                 if self.add:
-                        print(f'Газировка и {self.add}')
+                    print(f'Газировка и {self.add}')
                 else:
-                        print('Обычная гозировка')
+                    print('Обычная гозировка')
+a = int(input("Какая газировка нужна?"))
+soda = Soda(add = "Крем")
+soda2 = Soda(add = None)
+if a == 1:
+    soda.show_my_drink()
+elif a == 2:
+    soda2.show_my_drink()
      
